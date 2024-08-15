c9cfab2c13fc35f9ee511984c5c62d906139e181
João Henrique Buso Bortolotto <joao.bortolotto@ifood.com.br>
2024-08-15 09:49:45 -0300

    first commit

---
+++ ForegroundService/app/src/main/java/com/example/foregroundservice/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.foregroundservice
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import com.example.foregroundservice.ui.theme.ForegroundServiceTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.POST_NOTIFICATIONS),
+                0
+            )
+        }
+
+        setContent {
+            ForegroundServiceTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    Button(onClick = {
+                        Intent(applicationContext, RunningService::class.java).also { intent ->
+                            intent.action = RunningService.Actions.START.name
+                            startService(intent)
+                        }
+                    }) {
+                        Text("Start Service")
+                    }
+                    Button(onClick = {
+                        Intent(applicationContext, RunningService::class.java).also { intent ->
+                            intent.action = RunningService.Actions.STOP.name
+                            startService(intent)
+                        }
+                    }) {
+                        Text("Stop Service")
+                    }
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String, modifier: Modifier = Modifier) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        ForegroundServiceTheme {
+            Greeting("Android")
+        }
+    }
+}
